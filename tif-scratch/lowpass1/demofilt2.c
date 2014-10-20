1cd1023333da84f327745f12ef5e0e66575f074a
tycheng <tycheng@sfu.ca>
2014-10-20 01:51:38 -0700

    modified...filter

--- tif-scratch/lowpass1/demofilt2.c
+++ tif-scratch/lowpass1/demofilt2.c
@@ -10,16 +10,40 @@ SF_INFO sf_info_out;
 
 #define BUFFERSIZE 512
 #define CUTOFF_LOW 300.0
-#define CUTOFF_HIGH 150.0
+#define CUTOFF_HIGH 100.0
+
+//#define SIMP
+#define OTH
+
+#define NZEROS 2
+#define NPOLES 2
+#define GAIN 2.255945996e+03
+
+#ifdef OTH
+	double cx[NZEROS+1];
+	double cy[NPOLES+1];
+	static double filter(double xv[NZEROS+1],double yv[NPOLES+1],double next)
+	{
+		xv[0]=xv[1];
+		xv[1]=xv[2];
+		xv[2]=next/GAIN;
+		yv[0]=yv[1];
+		yv[1]=yv[2];
+		yv[2]=(xv[2] + xv[0]) + 2*xv[1] + (-0.9413432994 * yv[0]) + (1.9395702074 * yv[1]); 
+		return yv[2];
+	}
+#endif
+
+
 main(){
-	int i, numCh;
+	int numCh;
 	sf_count_t total,read_len;
 
 	float din[BUFFERSIZE*2];
 	float dout[BUFFERSIZE*2];
 	float tmp[BUFFERSIZE*2];
 
-	FILE *out;
+	FILE *out, *out_tmp;
 
 	sf_inp=sf_open("clip00.wav",SFM_READ,&sf_info_inp);
 	numCh = sf_info_inp.channels;
@@ -31,6 +55,7 @@ main(){
 	sf_info_out.format=sf_info_inp.format;
 
 	sf_out=sf_open("output.wav",SFM_WRITE,&sf_info_out);
+	out_tmp = fopen("levels.out", "w");
 	out = fopen("outlevels.out", "w");
 
 	float RC_low = 1.0/(2.0*M_PI*CUTOFF_LOW);
@@ -41,41 +66,76 @@ main(){
 	
 	total=0;
 
-	while(total<sf_info_inp.frames){
-		read_len=sf_readf_float(sf_inp,din,BUFFERSIZE);
-		//printf("read_len: %d, ", (int)read_len);
-		/*dout[0] = din[0];
-		dout[1] = din[1];
-		//low pass
-		/*for(i=2;i<read_len*2;i++){
-			dout[i] = dout[i-2] + (alpha_low*(din[i] - dout[i-2]));
-			dout[i+1] = dout[i-1] + (alpha_low*(din[i+1] - dout[i-1]));
-		}*/
+	while(total < sf_info_inp.frames){
+
 		float frameRMS = 0;
 		float frameSum = 0;
+		float channelSum;
+		int ch, f;
 
-		//low pass
-		tmp[0] = din[0];
-		tmp[1] = din[1];
-		for(i = numCh; i < (read_len*numCh) - 1; i+=2){
-			tmp[i] = tmp[i-2] + (alpha_low*(din[i] - tmp[i-2]));
-			tmp[i+1] = tmp[i-1] + (alpha_low*(din[i+1] - tmp[i-1]));
-		}
-		//high pass
-		dout[0] = tmp[0];
-		dout[1] = tmp[1];
-		for(i = numCh; i < (read_len*numCh) - 1; i+=2){
-			dout[i] = alpha_high*(dout[i-2] + tmp[i] - tmp[i-2]);
-			dout[i+1] = alpha_high*(dout[i-1] + tmp[i+1] - tmp[i-1]);
-			//average two channels and square?
-			frameSum += (dout[i]*dout[i]) + (dout[i+1]*dout[i+1]);
-		}
+		//tmp
+		float frameRMS_t = 0;
+		float frameSum_t = 0;
+		float channelSum_t;
+
+		read_len = sf_readf_float(sf_inp, din, BUFFERSIZE);
+		//printf("read_len: %d, ", (int)read_len);
+		
+		#ifdef SIMP
+			//low pass
+			tmp[0] = din[0];
+			tmp[1] = din[1];
+			for(f = 1; f < read_len; f++){
+				for(ch = 0; ch < numCh; ch++){
+					tmp[f*numCh+ch] = tmp[(f*numCh+ch) - numCh] + (alpha_low*(din[f*numCh+ch] - tmp[(f*numCh+ch) - numCh]));
+					channelSum_t += din[f*numCh+ch];
+				}
+				frameSum_t += channelSum_t * channelSum_t;
+			}
+			//high pass
+			dout[0] = tmp[0];
+			dout[1] = tmp[1];
+			channelSum = dout[0];
+			for(f = 1; f < read_len; f++){
+				for(ch = 0; ch < numCh; ch++){
+					dout[f*numCh+ch] = alpha_high*(dout[(f*numCh+ch) - numCh] + tmp[f*numCh+ch] - tmp[(f*numCh+ch)-numCh]);
+					channelSum += dout[f*numCh+ch];
+				}
+				//RMS
+				frameSum += channelSum * channelSum;
+			}
+
+
+			sf_writef_float(sf_out,dout,read_len);
+
+			//tmp
+			frameRMS_t = frameSum_t/read_len;
+			frameRMS_t = sqrt(frameRMS_t);
+			fprintf(out_tmp, "%12.10f", frameRMS_t);
+			fprintf (out_tmp, "\n") ;
+			//
+		#endif
+
+		#ifdef OTH
+			for(f = 0; f < read_len; f++){
+				for(ch = 0; ch < numCh; ch++){
+					dout[f*numCh+ch] = 2.0*filter(cx, cy, din[f*numCh+ch]);
+					channelSum += dout[f*numCh+ch];
+				}
+				frameSum += channelSum * channelSum;
+			}
+			sf_writef_float(sf_out,dout,read_len);
+		#endif
+
 		frameRMS = frameSum/read_len;
 		frameRMS = sqrt(frameRMS);
-		fprintf(out, "%12.10f\n", frameRMS);
-		sf_writef_float(sf_out,dout,read_len);
+		fprintf(out, "%12.10f", frameRMS);
+		fprintf (out, "\n") ;
+
 		total+=read_len;
 	}
 	sf_close(sf_inp);
 	sf_close(sf_out);
+	fclose(out);
+	fclose(out_tmp);
 }
