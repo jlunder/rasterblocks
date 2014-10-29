aa2d72b0e8b7a1ef40ba1c21e1ea140af4910119
rdone <rdone@sfu.ca>
2014-10-29 01:14:12 -0700

    Moved FFT code into it's own module, to be called by audio_analysis.c

--- stage-lights/filters.h
+++ stage-lights/filters.h
@@ -1,3 +1,7 @@
+#ifndef FILTERS_H_INCLUDED
+#define FILTERS_H_INCLUDED
 #include "stage_lights.h"
 
 void filter_analyze(SLRawAudio const * audio, SLAnalyzedAudio * analysis, int num_frames, int channels);
+
+#endif
