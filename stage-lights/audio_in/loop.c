b495f19222e07b2784d65ec698d9c06a69490341
Chris Tolliday <ctolliday@gmail.com>
2014-10-29 19:12:13 -0700

    Separated ALSA helpers to alsa_in.h, added to harness and changed loop.c to use alsa_in.h, currently some problems using it in the harness

--- stage-lights/audio_in/loop.c
+++ stage-lights/audio_in/loop.c
@@ -1,179 +1,26 @@
-/*
- * Copyright (c) 2012 Daniel Mack
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- */
+
+#include <signal.h>
+
+#define slError(...) printf(__VA_ARGS__);
+#define slFatal(...) printf(__VA_ARGS__);
+#define slInfo(...) printf(__VA_ARGS__);
+#include "alsa_in.h"
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <alsa/asoundlib.h>
- #include <signal.h>
-
-// HW params
-// these are the only parameters that work for the focusrite 2i2
-static unsigned int rate = 44100;
-static unsigned int format = SND_PCM_FORMAT_S32_LE;
-static unsigned int channels = 2;
-
-#define BUFFER_SIZE (512)
-int buf[BUFFER_SIZE * 2];
-snd_pcm_t *playback_handle, *capture_handle;
-
-static int open_stream(snd_pcm_t **handle, const char *name, int dir)
-{
-	snd_pcm_hw_params_t *hw_params;
-	snd_pcm_sw_params_t *sw_params;
-	const char *dirname = (dir == SND_PCM_STREAM_PLAYBACK) ? "PLAYBACK" : "CAPTURE";
-	int err;
-
-	if ((err = snd_pcm_open(handle, name, dir, 0)) < 0) {
-		fprintf(stderr, "%s (%s): cannot open audio device (%s)\n", 
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-	   
-	if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
-		fprintf(stderr, "%s (%s): cannot allocate hardware parameter structure(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-			 
-	if ((err = snd_pcm_hw_params_any(*handle, hw_params)) < 0) {
-		fprintf(stderr, "%s (%s): cannot initialize hardware parameter structure(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-
-	if ((err = snd_pcm_hw_params_set_access(*handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set access type(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-
-	if ((err = snd_pcm_hw_params_set_format(*handle, hw_params, format)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set sample format(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-
-	if ((err = snd_pcm_hw_params_set_rate_near(*handle, hw_params, &rate, NULL)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set sample rate(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-
-	if ((err = snd_pcm_hw_params_set_channels(*handle, hw_params, channels)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set channel count(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-
-	if ((err = snd_pcm_hw_params(*handle, hw_params)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set parameters(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-
-	snd_pcm_hw_params_free(hw_params);
-
-	if ((err = snd_pcm_sw_params_malloc(&sw_params)) < 0) {
-		fprintf(stderr, "%s (%s): cannot allocate software parameters structure(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-	if ((err = snd_pcm_sw_params_current(*handle, sw_params)) < 0) {
-		fprintf(stderr, "%s (%s): cannot initialize software parameters structure(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-	if ((err = snd_pcm_sw_params_set_avail_min(*handle, sw_params, BUFFER_SIZE)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set minimum available count(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-	if ((err = snd_pcm_sw_params_set_start_threshold(*handle, sw_params, 0U)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set start mode(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-	if ((err = snd_pcm_sw_params(*handle, sw_params)) < 0) {
-		fprintf(stderr, "%s (%s): cannot set software parameters(%s)\n",
-			name, dirname, snd_strerror(err));
-		return err;
-	}
-
-	return 0;
-}
 void intHandler(int signal) {
-	fprintf(stdout, "Closing playback/capture devices\n");
-	snd_pcm_close(playback_handle);
-	snd_pcm_close(capture_handle);
+	slAlsaClose();
 	exit(0);
 }
-  
 int main(int argc, char *argv[])
 {
 	signal(SIGINT, intHandler);
 
-	int err;
-
-	if ((err = open_stream(&playback_handle, argv[1], SND_PCM_STREAM_PLAYBACK)) < 0)
-		return err;
-
-	if ((err = open_stream(&capture_handle, argv[2], SND_PCM_STREAM_CAPTURE)) < 0)
-		return err;
-
-	if ((err = snd_pcm_prepare(playback_handle)) < 0) {
-		fprintf(stderr, "cannot prepare audio interface for use(%s)\n",
-			 snd_strerror(err));
-		return err;
-	}
-	
-	if ((err = snd_pcm_start(capture_handle)) < 0) {
-		fprintf(stderr, "cannot prepare audio interface for use(%s)\n",
-			 snd_strerror(err));
-		return err;
-	}
-
-	memset(buf, 0, sizeof(buf));
-
+	slAlsaInit(argv[1],argv[2],480,2);
 	while (1) {
-		int avail;
-
-		if ((err = snd_pcm_wait(playback_handle, 1000)) < 0) {
-			fprintf(stderr, "poll failed(%s)\n", strerror(errno));
-			break;
-		}
-
-		avail = snd_pcm_avail_update(capture_handle);
-		if (avail > 0) {
-			if (avail > BUFFER_SIZE)
-				avail = BUFFER_SIZE;
-
-			snd_pcm_readi(capture_handle, buf, avail);
-		}
-
-		avail = snd_pcm_avail_update(playback_handle);
-		if (avail > 0) {
-			if (avail > BUFFER_SIZE)
-				avail = BUFFER_SIZE;
-
-			snd_pcm_writei(playback_handle, buf, avail);
-		}
+		slAlsaRead();
 	}
+	slAlsaClose();
 
-	snd_pcm_close(playback_handle);
-	snd_pcm_close(capture_handle);
 	return 0;
 }
+
