24e9b3748381e27312082973cc29c937f6680e76
Joseph Lunderville <jlunderv@sfu.ca>
2014-11-07 03:56:06 -0800

    Tweak light gen code. Was going to do something more fancy but this will probably look good enough, yay.

--- stage-lights/light_generation.c
+++ stage-lights/light_generation.c
@@ -1,6 +1,56 @@
 #include "light_generation.h"
 
 
+/*
+static SLColor g_slVuPalette[5] = {
+    {  0, 255,   0, 0},
+    {  0, 255,   0, 0},
+    {  0, 255,   0, 0},
+    {  0, 255,   0, 0},
+    {255,   0,   0, 0},
+};
+*/
+
+static SLColor g_slWarmPalette[5] = {
+    {  0,   0,   0, 0},
+    {127,   0,   0, 0},
+    {127,  95,   0, 0},
+    {255, 191,   0, 0},
+    {255, 255,  63, 0},
+};
+
+static SLColor g_slColdPalette[5] = {
+    {  0,   0,   0, 0},
+    {  0,  63, 255, 0},
+    { 63,   0,  63, 0},
+    {  0, 255,  63, 0},
+    {255, 255, 255, 0},
+};
+
+
+static SLColor getPaletteF(SLColor const palette[5], float f)
+{
+    if(f < 0.0f) {
+        return palette[0];
+    }
+    else if(f < 1.00f) {
+        float a = fmodf(f * 4.0f, 1.0f);
+        float b = 1.0f - a;
+        size_t i = (size_t)(f * 4.0f);
+        SLColor c = {
+            (uint8_t)(palette[i].r * b + palette[i + 1].r * a),
+            (uint8_t)(palette[i].g * b + palette[i + 1].g * a),
+            (uint8_t)(palette[i].b * b + palette[i + 1].b * a),
+            0
+        };
+        return c;
+    }
+    else {
+        return palette[4];
+    }
+}
+
+
 void slLightGenerationInitialize(SLConfiguration const * config)
 {
     UNUSED(config);
@@ -35,19 +85,20 @@ static inline float getBrightness(SLColor const * c)
 void slLightGenerationGenerate(SLAnalyzedAudio const * analysis,
     SLLightData * lights)
 {
-	setBrightness(&lights->left[0], analysis->bassEnergy);
-	for(int i = 1; i < SL_NUM_LIGHTS_LEFT; ++i) {
-		setBrightness(&lights->left[i], getBrightness(&lights->left[i-1])*0.9);
+    float leftTreble = analysis->trebleEnergy * sqrtf(1.0f - analysis->leftRightBalance);
+    float rightTreble = analysis->trebleEnergy * sqrtf(analysis->leftRightBalance);
+    float bass = analysis->bassEnergy;
+    
+	for(int i = 0; i < SL_NUM_LIGHTS_LEFT; ++i) {
+		lights->left[i] = getPaletteF(g_slColdPalette, leftTreble * powf(0.7f, i));
 	}
 	
-	setBrightness(&lights->right[0], analysis->midEnergy);
-	for(int i = 1; i < SL_NUM_LIGHTS_RIGHT; ++i) {
-		setBrightness(&lights->right[i], getBrightness(&lights->right[i-1])*0.9);
+	for(int i = 0; i < SL_NUM_LIGHTS_RIGHT; ++i) {
+		lights->right[i] = getPaletteF(g_slColdPalette, rightTreble * powf(0.7f, i));
 	}
 	
-	setBrightness(&lights->overhead[0], analysis->trebleEnergy);
-	for(int i = 1; i < SL_NUM_LIGHTS_OVERHEAD; ++i) {
-		setBrightness(&lights->overhead[i], getBrightness(&lights->overhead[i-1])*0.9);
+	for(int i = 0; i < SL_NUM_LIGHTS_OVERHEAD; ++i) {
+		lights->overhead[i] = getPaletteF(g_slWarmPalette, bass * powf(0.7f, i));
 	}
 }
 
